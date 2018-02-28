f32ce75d84531ac581f495284eec32ec4cde966c
spemer <ghsspower@naver.com>
2018-03-01 04:08:13 +0900

    test

--- 2018/test/test/main.c
+++ 2018/test/test/main.c
@@ -7,9 +7,30 @@
 //
 
 #include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+#pragma warning (disable:4996)
+void Year(void);
+int main(void)
+{
+    int a;
+    do
+    {
+        Year();
+        printf("\n다시 하시겠습니까?\nYes: 1, No: 0\n");
+        scanf("%d", &a);
+    } while (a == 1);
     return 0;
+}
+void Year(void)
+{
+    int year;
+    printf("연도를 입력하세요: ");
+    scanf("%d", &year);
+    if (year % 4 == 0 & year % 100 != 0 || year % 400 == 0)
+    {
+        printf("%d년은 윤년입니다\n", year);
+    }
+    else
+    {
+        printf("%d년은 윤년이 아닙니다\n", year);
+    }
 }
