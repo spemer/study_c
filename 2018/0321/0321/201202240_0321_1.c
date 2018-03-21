a529d90b082f72ab63efa00a51c77363a1f8efb1
spemer <ghsspower@naver.com>
2018-03-21 09:45:16 +0900

    0321

---
+++ 2018/0321/0321/201202240_0321_1.c
@@ -0,0 +1,36 @@
+//
+//  main.c
+//  0321
+//
+//  Created by Hyouk Seo on 2018. 3. 21..
+//  Copyright © 2018년 Hyouk Seo. All rights reserved.
+//
+//  정수형 배열 10개 선언
+//  배열에 값 대입
+//  -> 입력 받기
+//  최대값 찾기
+//  인덱스, 최대값 출력
+//  이러닝에 제출하기
+
+#include <stdio.h>
+#define NUM 10
+//int main(int argc, const char * argv[]) {
+//    // insert code here...
+//    int tmp, i, n;
+//    int score[10];
+//    tmp = score[0];
+//    for(i = 0; i < NUM; i++) {
+//        printf("정수를 입력하세요: ");
+//        scanf("%d", &n);
+//        score[i] = n;
+//        if(score[i] > tmp) {
+//            tmp = score[i];
+//        }
+//    }
+//    for(i = 0; i < NUM; i++) {
+//        printf("인덱스 %d: %d\n", i, score[i]);
+//    }
+//    printf("최대값: %d\n", tmp);
+//    return 0;
+//}
+
---
+++ 2018/0321/0321/201202240_0321_2.c
@@ -0,0 +1,9 @@
+//
+//  201202240_0321_2.c
+//  0321
+//
+//  Created by Hyouk Seo on 2018. 3. 21..
+//  Copyright © 2018년 Hyouk Seo. All rights reserved.
+//
+
+#include <stdio.h>
