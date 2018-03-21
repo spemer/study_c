93e64bd8a78cf05a12c4b8ac5e1c796d753214d6
spemer <ghsspower@naver.com>
2018-03-21 11:17:29 +0900

    swap

--- 2018/0321/0321/201202240_0321_2.c
+++ 2018/0321/0321/201202240_0321_2.c
@@ -7,3 +7,16 @@
 //
 
 #include <stdio.h>
+void swap (int *px, int *py) {
+    int tmp;
+    tmp = *px;
+    *px = *py;
+    *py = tmp;
+}
+int main (void) {
+    int a = 1, b = 2;
+    printf("swap을 호출하기 전: a: %d, b: %d\n", a, b);
+    swap(&a, &b);
+    printf("swap을 호출한 후: a: %d, b: %d\n", a, b);
+    return 0;
+}
