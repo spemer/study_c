8561f7a76db1c5184daca1adc0809950ed8e028a
spemer <ghsspower@naver.com>
2018-05-13 17:28:19 +0900

    save

---
+++ 2018/0420/0420-1/0420-1/main.c
@@ -0,0 +1,28 @@
+//
+//  main.c
+//  0420-1
+//
+//  Created by Hyouk Seo on 2018. 4. 20..
+//  Copyright © 2018년 Hyouk Seo. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, const char * argv[]) {
+    char s[] = "abcdefgh";
+    int len = strlen(s);
+    printf("length of %s: %d\n", s, len);
+    
+//    char name[100];
+//    char address[100];
+//    
+//    printf("name: ");
+//    scanf("%s", name);
+//    
+//    printf("address: ");
+//    scanf("%s", address);
+//    
+//    printf("Hi %s in %s\n", name, address);
+    return 0;
+}
